3a42f9a241397d0a8c4b43c405a205b13e8d6b74
blakehawkins <blake.hawkins.11@gmail.com>
2020-01-11 17:01:22 +0000

    working requests

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::env::args;
 use std::io::Error;
 
 use futures::future::Future;
+use gm_types::messages::Message;
 use glitch_in_the_matrix::request::MatrixRequestable;
+use glitch_in_the_matrix::room::{NewRoom, RoomClient};
 use glitch_in_the_matrix::MatrixClient;
 use tokio_core::reactor::Core;
+use urlencoding::encode;
+
 
 fn main() -> Result<(), std::io::Error> {
     let mut core = Core::new()?;
@@ -16,7 +20,14 @@ fn main() -> Result<(), std::io::Error> {
             .expect("Missing password argument"),
         "https://matrix.org",
         &core.handle()
-    ).map(|client| println!("{:?}", client.get_url()));
+    ).and_then(|mut client| {
+        NewRoom::from_alias(&mut client, &encode("#_hack:matrix.org")).map(|room| (client, room))
+    }).and_then(|(mut client, room)| {
+        RoomClient {
+            room: &room,
+            cli: &mut client
+        }.send(Message::Text { body: "hello world".into(), formatted_body: None, format: None })
+    });
 
     core.run(txns).map_err(|merr| {
         println!("{}", merr);
