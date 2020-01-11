b9c09d90cefc0121d2deca4479a662a2d1dccf0c
blakehawkins <blake.hawkins.11@gmail.com>
2020-01-11 15:46:13 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use glitch_in_the_matrix::MatrixClient;
+use hyper::Client;
+use hyper_openssl::HttpsConnector;
+use tokio_core::reactor::Core;
+
+fn main() -> Result<(), std::io::Error> {
+    let core = Core::new()?;
+    let https = HttpsConnector::new(10)?;
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let fut = MatrixClient::login_password(
+        "crates.io".into(),
+        "password".into(),
+        "matrix.org",
+        &core.handle()
+    );
+    
+    Ok(())
+}
