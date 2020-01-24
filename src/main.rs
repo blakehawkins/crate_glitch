09503ac98d0f194c9339feffa37c08320ab10639
blakehawkins <blake.hawkins.11@gmail.com>
2020-01-24 23:01:27 +0000

    apply rustfmt and clippy

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,20 @@ use std::env::args;
 use futures::future::{ok, Future};
 use futures::stream;
 use futures::stream::Stream;
-use gm_types::content::Content;
-use gm_types::messages::Message;
-use gm_types::replies::SendReply;
-use gm_types::room::Room;
-use gm_types::sync::{JoinedRoom, SyncReply};
 use glitch_in_the_matrix::errors::MatrixError;
 use glitch_in_the_matrix::request::MatrixRequestable;
 use glitch_in_the_matrix::room::{NewRoom, RoomClient};
 use glitch_in_the_matrix::sync::SyncStream;
 use glitch_in_the_matrix::MatrixClient;
-use serde::{Serialize, Deserialize};
+use gm_types::content::Content;
+use gm_types::messages::Message;
+use gm_types::replies::SendReply;
+use gm_types::room::Room;
+use gm_types::sync::{JoinedRoom, SyncReply};
+use serde::{Deserialize, Serialize};
 use serde_yaml;
 use tokio_core::reactor::Core;
 use urlencoding::encode;
-
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 struct Config {
@@ -29,112 +28,123 @@ struct Config {
     prepend_with: String,
 }
 
-
 fn into_sends<T: MatrixRequestable + 'static>(
     jroom: &JoinedRoom,
     room_client: &mut RoomClient<T>,
     listen_to: String,
     prepend_with: String,
-) -> Vec<impl Future<Item=SendReply, Error=MatrixError> + 'static> {
-    jroom.timeline.events.iter().map(move |event| {
-        match &event.content {
-            Content::RoomMessage(message) => {
-                match message {
-                    Message::Text { body, formatted_body: _, format: _ } => {
-                        if body.starts_with(&listen_to) {
-                            let crate_name = &body[listen_to.len()..];
+) -> Vec<impl Future<Item = SendReply, Error = MatrixError> + 'static> {
+    jroom
+        .timeline
+        .events
+        .iter()
+        .filter_map(move |event| match &event.content {
+            Content::RoomMessage(message) => match message {
+                Message::Text {
+                    body,
+                    ..
+                } => {
+                    if body.starts_with(&listen_to) {
+                        let crate_name = &body[listen_to.len()..];
 
-                            println!("{}", crate_name);
+                        println!("{}", crate_name);
 
-                            Some(room_client.send_simple(
-                                format!("{}{}", prepend_with, crate_name)
-                            ).map_err(|e| { println!("send_err: {}", e); e }))
-                        } else {
-                            None
-                        }
-                    },
-                    _ => None,
+                        Some(
+                            room_client
+                                .send_simple(format!("{}{}", prepend_with, crate_name))
+                                .map_err(|e| {
+                                    println!("send_err: {}", e);
+                                    e
+                                }),
+                        )
+                    } else {
+                        None
+                    }
                 }
+                _ => None,
             },
             _ => None,
-        }
-    }).filter_map(|f| f).collect()
+        })
+        .collect()
 }
 
 fn send_stream(
     (mut client, room): (MatrixClient, Room<'static>),
     listen_to: String,
     prepend_with: String,
-) -> Box<dyn Stream<Item=impl Future<Item=SendReply, Error=MatrixError>, Error=MatrixError>> {
-    Box::new(SyncStream::new(client.clone()).map(move |freply: SyncReply| {
-        let mut rc = RoomClient { room: &room, cli: &mut client };
+) -> Box<dyn Stream<Item = impl Future<Item = SendReply, Error = MatrixError>, Error = MatrixError>>
+{
+    Box::new(
+        SyncStream::new(client.clone())
+            .map(move |freply: SyncReply| {
+                let mut rc = RoomClient {
+                    room: &room,
+                    cli: &mut client,
+                };
 
-        let futs = if let Some(jroom) = freply.rooms.join.get(&room) {
-            into_sends(jroom, &mut rc, listen_to.clone(), prepend_with.clone())
-        } else {
-            vec![]
-        };
-        
-        stream::iter_ok(futs.into_iter())
-    }).flatten())
+                let futs = if let Some(jroom) = freply.rooms.join.get(&room) {
+                    into_sends(jroom, &mut rc, listen_to.clone(), prepend_with.clone())
+                } else {
+                    vec![]
+                };
+
+                stream::iter_ok(futs.into_iter())
+            })
+            .flatten(),
+    )
 }
-
 
 #[allow(unused_mut)]
 fn main() -> Result<(), std::io::Error> {
     let mut core = Core::new()?;
 
-    let args: Config = serde_yaml::from_reader(
-        std::fs::File::open(args().nth(1).unwrap_or("config.yaml".into()))?
-    ).expect("Config file was not deserialisable.");
+    let args: Config = serde_yaml::from_reader(std::fs::File::open(
+        args().nth(1).unwrap_or_else(|| "config.yaml".into()),
+    )?)
+    .expect("Config file was not deserialisable.");
     let args2 = args.clone();
 
     let handle = core.handle();
     let handle2 = core.handle();
     let rm = args.room.clone();
 
-    let txns = MatrixClient::new_from_access_token(
-        &args.token,
-        "https://matrix.org",
-        &handle.clone()
-    ).or_else(move |mut _e| {
-        let handle2 = handle2.clone();
-        let args = args.clone();
+    let txns =
+        MatrixClient::new_from_access_token(&args.token, "https://matrix.org", &handle)
+            .or_else(move |mut _e| {
+                let handle2 = handle2.clone();
 
-        MatrixClient::login_password(
-            &args.account,
-            &args.password,
-            "https://matrix.org",
-            &handle2
-        )
-    }).and_then(move |mut client| {
-        let rm = rm.clone();
-        
-        println!("Access token: {}", client.get_access_token());
+                MatrixClient::login_password(
+                    &args.account,
+                    &args.password,
+                    "https://matrix.org",
+                    &handle2,
+                )
+            })
+            .and_then(move |mut client| {
+                println!("Access token: {}", client.get_access_token());
 
-        NewRoom::from_alias(
-            &mut client,
-            &encode(&rm)
-        ).map(move |room| (client, room))
-    })
-    .into_stream()
-    .map(move |pair| {
-        let args2 = args2.clone();
+                NewRoom::from_alias(&mut client, &encode(&rm)).map(move |room| (client, room))
+            })
+            .into_stream()
+            .map(move |pair| {
+                let args2 = args2.clone();
 
-        send_stream(pair, args2.listen_to, args2.prepend_with)
-    })
-    .map_err(|e| println!("send_stream err: {:?}", e));
+                send_stream(pair, args2.listen_to, args2.prepend_with)
+            })
+            .map_err(|e| println!("send_stream err: {:?}", e));
 
     let handle = core.handle();
- 
+
     let res = txns.for_each(move |mut syncs| {
         let handle = handle.clone();
 
-        syncs.map_err(|e| { println!("syncs_err: {:?}", e) }).for_each(move |txn| {
-            handle.spawn(txn.map(|_| ()).map_err(|e| println!("txn err: {:?}", e)));
+        syncs
+            .map_err(|e| println!("syncs_err: {:?}", e))
+            .for_each(move |txn| {
+                handle.spawn(txn.map(|_| ()).map_err(|e| println!("txn err: {:?}", e)));
 
-            ok(())
-        })
+                ok(())
+            })
     });
 
     core.run(res).expect("Unresolved errors encountered.");
